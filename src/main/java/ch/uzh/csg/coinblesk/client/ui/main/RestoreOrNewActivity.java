6f29dfa7e4962605e106f370e12ea1316f444bb1
Raphael <r.voellmy@gmail.com>
2015-07-28 15:18:55 +0200

    Finish setup activity after main activity is started Disable history for setup activity

--- src/main/java/ch/uzh/csg/coinblesk/client/ui/main/RestoreOrNewActivity.java
+++ src/main/java/ch/uzh/csg/coinblesk/client/ui/main/RestoreOrNewActivity.java
@@ -201,6 +201,7 @@ public class RestoreOrNewActivity extends WalletActivity {
         Intent intent = new Intent(RestoreOrNewActivity.this, MainActivity.class);
         dismissProgressDialog();
         startActivity(intent);
+        finish();
     }
 
 }
